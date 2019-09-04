36270047b025bed594a84fcf4613fea7218596b7
CleoZhang <forest-zhang@hotmail.com>
2019-09-04 19:16:07 +0000

    Set up the express server

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// 1. check if the server is response-able / successfully set 
+app.get("/",(request, response) => {
+  response.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
